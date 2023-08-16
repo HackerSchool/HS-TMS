9b1e4e811a6f09f93d302e6eeafbf3f7148924b2
fmata97 <fabiomata97@gmail.com>
2023-08-16 20:17:42 +0100

    Dashboard layout done

---
+++ frontend/src/components/RecentTransactionsTable.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import CircularProgress from '@mui/material/CircularProgress';
+
+export default function RecentTransactionsTable({ data, loading }) {
+    const rowsPerPage = 3;
+
+    // Fill with empty rows if there are less that "rowsPerPage" transactions
+    const emptyRows = Math.max(0, rowsPerPage - data.length);
+
+    function formatString(str) {
+        let newStr = str;
+
+        if (str.length > 40) {
+            newStr = str.slice(0,40);
+            newStr += "...";
+        }
+        return newStr;
+    }
+
+    return (
+        <TableContainer>
+            <Table sx={{ minWidth: 300, width: "100%" }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="center" padding="none">Date</TableCell>
+                        <TableCell align="center" padding="none">Value</TableCell>
+                        <TableCell align="center" padding="none">Balance</TableCell>
+                        <TableCell align="center" >Projects</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.length === 0 && !loading && ( // display message when there's no data to display
+                        <TableRow>
+                            <TableCell colSpan={4} align="center" sx={{ fontSize: 18, border: 0}}>
+                                No transactions found
+                            </TableCell>
+                        </TableRow>
+                    )}
+
+                    {loading && (
+                    <TableRow>
+                        <TableCell colSpan={4} align="center" sx={{ border: 0 }} >
+                            <CircularProgress className="loading-circle large" sx={{ m: 0}} />
+                        </TableCell>
+                    </TableRow>
+                    )}
+
+                    {!loading && data.map((row) => (
+                            <TableRow
+                                key={`${row.date}+${row.value}+${Math.random()}`}
+                                hover
+                            >
+                                <TableCell component="th" scope="row" align="center">
+                                    {row.date}
+                                </TableCell>
+                                <TableCell align="center">{`${row.value}€`}</TableCell>
+                                <TableCell align="center">{`${row.balance}€`}</TableCell>
+                                <TableCell align="center">
+                                    {row.projects ? formatString(row.projects) : "-"}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    {!loading && emptyRows > 0 && (
+                        <TableRow style={{ height: 30 * emptyRows }}>
+                            <TableCell colSpan={4} sx={{border:0}} />
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,127 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../styles/Dashboard.css'
+import axios_instance from '../Axios';
+import DashboardCard from '../components/DashboardCard';
+import RecentTransactionsTable from '../components/RecentTransactionsTable';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import PersonIcon from '@mui/icons-material/Person';
+import NotificationAddIcon from '@mui/icons-material/NotificationAdd';
 
 function DashboardPage() {
+
+    // Projects overview stats
+    const [totalBalance, setTotalBalance] = useState();
+    const [activeProjects, setActiveProjects] = useState();
+    const [transactionsLastMonth, setTransactionsLastMonth] = useState();
+    const [authorizedUsersNumber, setAuthorizedUsersNumber] = useState();
+    
+    // Reminders
+    const [reminders, setReminders] = useState([]);
+    const [remindersLoading, setRemindersLoading] = useState(true);
+
+    useEffect(() => {
+        axios_instance.get("reminders")
+            .then(res => {
+                if (res.status === 200) return res.data;
+                else throw new Error();
+            })
+            .then(data => setReminders(data)) // FIXME
+            .catch(err => {
+                console.log(err); // FIXME
+            })
+            .finally(() => setRemindersLoading(false));
+    })
+
+    // Balance chart
+    const [chartData, setChartData] = useState();
+    
+    // Transactions to display on the recent transactions table
+    const [transactions, setTransactions] = useState([]);
+    const [transactionsLoading, setTransactionsLoading] = useState(true);
+
+    useEffect(() => {
+        axios_instance.get("transactions")
+            .then(res => {
+                if (res.status === 200) return res.data;
+                else throw new Error();
+            })
+            .then(data => setTransactions(data.slice(0,4))) // FIXME
+            .catch(err => {
+                console.log(err); // FIXME
+            })
+            .finally(() => setTransactionsLoading(false));
+    })
+
+
+
     return (
         <section className="page" id='DashboardPage'>
-            Dashboard
+            <div className="dashboard-row">
+                <div className="dashboard-item" id='dashboard-project-overview'>
+                    <h3 className='dashboard-item-title'>Projects Overview</h3>
+                    <div className="dashboard-item-content">
+                        <div className="dashboard-card-row">
+                            <DashboardCard
+                                mainText="€ 1000"
+                                subText="Total balance"
+                                green
+                                bolderMain
+                            />
+                            <DashboardCard
+                                headingText="8"
+                                mainText="Active"
+                                subText="Projects"
+                                icon={<TaskAltIcon />}
+                            />
+                        </div>
+                        <div className="dashboard-card-row">
+                            <DashboardCard
+                                headingText="3"
+                                mainText="Transactions"
+                                subText='in the past month'
+                                smallerMain
+                            />
+                            <DashboardCard
+                                headingText="60"
+                                mainText="Active"
+                                subText="members"
+                                icon={<PersonIcon />}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <hr className='vl' />
+
+                <div className="dashboard-item" id='dashboard-reminders'>
+                    <div className="reminders-title-group">
+                        <h3 className='dashboard-item-title'>Reminders</h3>
+                        <NotificationAddIcon sx={{ cursor: 'pointer' }} />
+                    </div>
+                    <div className="dashboard-item-content">
+                        No reminders found
+                    </div>
+                </div>
+            </div>
+
+            <div className="dashboard-row last">
+                <div className="dashboard-item" id='dashboard-balance'>
+                    <h3 className='dashboard-item-title'>Balance over the last 30 days</h3>
+                    <div className="dashboard-item-content"></div>
+                </div>
+
+                <hr className='vl' />
+
+                <div className="dashboard-item" id='dashboard-recent-transactions'>
+                    <h3 className='dashboard-item-title'>Recent Transactions</h3>
+                    <div className="dashboard-item-content">
+                        <RecentTransactionsTable
+                            data={transactions}
+                            loading={transactionsLoading}
+                        />
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
