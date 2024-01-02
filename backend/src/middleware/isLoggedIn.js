8ecc799dbc782bab8ae5a68b29406aee46c8f298
Nuno Gonçalves <nunomrgoncalves03@gmail.com>
2024-01-02 17:57:30 +0000

    Add try-catch block to handle errors in isLoggedIn

--- backend/src/middleware/isLoggedIn.js
+++ backend/src/middleware/isLoggedIn.js
@@ -1,25 +1,29 @@
 const User = require("../models/User");
 
 async function isLoggedIn(req, res, next) {
-	if (req.user) {
-		if (await User.getOne(require("../models/pool"), req.user.username)) {
-			await User.updateOne(
-				require("../models/pool"),
-				req.user.username,
-				true,
-				req.user.name,
-				req.user.photo,
-				req.user.email
-			);
+	try {
+		if (req.user) {
+			if (await User.getOne(require("../models/pool"), req.user.username)) {
+				await User.updateOne(
+					require("../models/pool"),
+					req.user.username,
+					true,
+					req.user.name,
+					req.user.photo,
+					req.user.email
+				);
 
-			next();
-		} else if (req.user.username === "demo") {
-			next();
+				next();
+			} else if (req.user.username === "demo") {
+				next();
+			} else {
+				res.sendStatus(403);
+			}
 		} else {
-			res.sendStatus(403);
+			res.sendStatus(401);
 		}
-	} else {
-		res.sendStatus(401);
+	} catch (error) {
+		next(error);
 	}
 }
 
