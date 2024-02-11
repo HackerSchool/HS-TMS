608b548cc54f0647f4d5f67fc122f34e6a302c3b
fmata97 <fabiomata97@gmail.com>
2024-02-11 19:21:18 +0000

    Fixed balance card text wrapping for big values and charts modebar posotion

--- frontend/src/components/DashboardCard.jsx
+++ frontend/src/components/DashboardCard.jsx
@@ -10,18 +10,18 @@ function DashboardCard({
   smallerMain = false,
 }) {
   return (
-    <div className={`dashboard-card ${green && "green"}`}>
-      <div className={`dashboard-card-text ${subText && "align-top"}`}>
-        <div className="dashboard-card-heading">{headingText}</div>
+    <div className={`dashboard-card ${green ? "green" : ""}`}>
+      <div className={`dashboard-card-text ${subText ? "align-top" : ""}`}>
+        {headingText && <div className="dashboard-card-heading">{headingText}</div>}
         <div
-          className={`dashboard-card-main ${bolderMain && "bolder"} ${smallerMain && "smaller"}`}
+          className={`dashboard-card-main ${bolderMain ? "bolder" : ""} ${smallerMain ? "smaller" : ""}`}
         >
           {mainText}
-          <div className="dashboard-card-sub">{subText}</div>
+          {subText && <div className="dashboard-card-sub">{subText}</div>}
         </div>
       </div>
 
-      <div className="dashboard-card-icon">{icon}</div>
+      {icon && <div className="dashboard-card-icon">{icon}</div>}
     </div>
   );
 }
